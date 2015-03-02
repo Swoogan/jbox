dd3876be995b5c26ba5aa8f0319f7fc809bb7bc9
Colin Svingen <swoogan@gmail.com>
2015-03-01 22:32:23 -0500

    Creating songs.json

--- scripts/createsongdb.py
+++ scripts/createsongdb.py
@@ -16,9 +16,9 @@
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
 import sys, os, time, mp3info
-from utilities import template, jsonfile, songs
+from utilities import template, jsonfile
 
-def walktree(path, dirid, recurse):
+def walktree(path, recurse):
   output = ''
   has_mp3s = 0
   if os.path.exists(path):
@@ -32,12 +32,11 @@ def walktree(path, dirid, recurse):
         mp3file.close()
 
         if info:
-          print info
           length = info['seconds']
         else:
           length = '0'
 
-        songdb.getTable('Songs').insert([dirid,item[:-4],fullpath,length])
+        songs.append({'song': item[:-4], 'path': fullpath, 'length': length})
         output += '<tr><td width=20></td></tr><tr><td width=30></td><td>"' + item + '" added</td></tr>\n'
         has_mp3s = 1
 
@@ -49,37 +48,29 @@ def walktree(path, dirid, recurse):
         fullpath = os.path.join(path, item)
         if os.path.isdir(fullpath) and item[:2] is not '..':
           output += '<tr><td colspan=2><b>Recending into subdirectory ' + item + ':</b></td></tr>\n'
-          output += walktree(fullpath, dirid, recurse)
+          output += walktree(fullpath, recurse)
 
   else:
     output += '<tr><td width=20></td><td>Directory does not exist</td></tr>\n'
 
   return output
 
-db = os.path.join('data','songdb')
-songdb = flatdb.Database()
-try:
-  songdb.connect(db)
-  songdb.getTable('Songs').truncate()
-except flatdb.DBError:
-#  songdb.create(db)
-  songdb.createTable('Songs',['ID','DIR_ID','SONG','PATH','LENGTH'])
+songs_file = 'songs.json'
+if os.path.exists(songs_file):
+  songs = jsonfile.load(songs_file)
+else:
+  songs = []  
 
-
-config = os.path.join('..','jbox.conf') 
+config = os.path.join('..', 'jbox.conf') 
 data = jsonfile.load(config)
 
 output = ''
 for path in data['directories']:
   output += '<tr><td colspan=2><h2>Processing ' + path + ':</h2></td></tr>\n'
   recurse = data['directories'][path]
-  output += walktree(path, 1, recurse)
+  output += walktree(path, recurse)
 
-try:
-  songdb.commit()
-except flatdb.TableError, err:
-  print >> sys.stderr, err
-
+jsonfile.save(songs_file, songs)
 
 tags = {'TABLE_CONTENTS':output}
 print template.populateTemplate(os.path.join('templates','createsongdb.tpl'),tags)
