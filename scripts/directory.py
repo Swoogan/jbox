10c336b3a52217af3d503ec0d6350e4f9c95b3b1
Colin Svingen <swoogan@gmail.com>
2015-03-01 20:35:55 -0500

    Converted directory.py to json

--- scripts/directory.py
+++ scripts/directory.py
@@ -16,53 +16,61 @@
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
 
-import cgi, sys, os, flatdb
-from utilities import template
+import cgi, sys, os, json
+from utilities import template, parseconf
+
+# songdb = flatdb.Database()
+
+# try:
+#   songdb.connect(os.path.join('data','songdb'))
+# except flatdb.DBError:
+#   songdb.create(os.path.join('data','songdb'))
+#   songdb.createTable('Dirs',['ID','DIR','RECURSIVE'])
 
 table = ''
 
-songdb = flatdb.Database()
+path = os.path.join('..','jbox.conf') 
+data = parseconf.load(path)
 
-try:
-  songdb.connect(os.path.join('data','songdb'))
-except flatdb.DBError:
-  songdb.create(os.path.join('data','songdb'))
-  songdb.createTable('Dirs',['ID','DIR','RECURSIVE'])
+if 'directories' not in data:
+  data['directories'] = {}
 
 form = cgi.FieldStorage()
+
 if form:
   if form.has_key('add') and form.has_key('newdir'):
     new = form['newdir'].value
+
+    recurse = False
+
     if form.has_key('recurse'):
-      recurse = form['recurse'].value
-    else:
-      recurse = 'N'
-    if os.path.exists(new):
-      songdb.getTable('Dirs').insert([new,recurse])
-    else:
-      table += '<p style="font-size: 14px; color: red;" align="center">ERROR: path \'' + new + '\' does not exist</p>'
-  elif form.has_key('del.x') and form.has_key('id'):
-    dirid = form['id'].value
-    try:
-      songdb.getTable('Dirs').delete(dirid)
-    except flatdb.TableError:
-      pass
+      recurse = True if form['recurse'].value == 'Y' else False
 
-  songdb.commit()
+    if new not in data['directories']:
+      if os.path.exists(new):
+          data['directories'].append({new: recurse})
+      else:
+        table += '<p style="font-size: 14px; color: red;" align="center">ERROR: path \'' + new + '\' does not exist</p>'
 
+  elif form.has_key('del.x') and form.has_key('path'):
+    path = form['path'].value
+    del data['directories'][path]
+
+  parseconf.save(path, data)
 
 table += '<table align="center" cellspacing="0" border width="80%">\n'     \
          '<tr align="center"><td>Existing Directorys</td><td>Recursive</td><td>Delete</td></tr>\n'
-for directory in songdb.getTable('Dirs').getIds():
-  dirinfo = songdb.getTable('Dirs').getRowById(directory)
-  table += '<tr><td>' + dirinfo['DIR'] + '</td>\n'    \
-           '<td align="center">' + dirinfo['RECURSIVE'] + '</td>\n'  \
+
+for path in data['directories']: 
+  recurse = str(data['directories'][path])
+  table += '<tr><td>' + path + '</td>\n'    \
+           '<td align="center">' + recurse + '</td>\n'  \
            '<td align="center"><form action="directory.py" method="post">'   \
-           '<input type="hidden" name="id" value="' + dirinfo['ID'] + '">'  \
+           '<input type="hidden" name="path" value="' + path + '">'  \
            '<input type="image" src="../images/delete.png" name="del" value="Delete"></td></form></tr>\n'
 
 table += '</table>\n'
 
 tags = {'TABLE':table}
-print template.populateTemplate(os.path.join('templates','directory.tpl'),tags)
+print template.populateTemplate(os.path.join('templates','directory.tpl'), tags)
 
