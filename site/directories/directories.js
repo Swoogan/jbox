f129c74057403f64e86d9fb070cf4ba22473dc04
Colin Svingen <swoogan@gmail.com>
2015-03-13 20:58:35 -0400

    Moved the css into the app.css

--- site/directories/directories.js
+++ site/directories/directories.js
@@ -5,6 +5,14 @@ angular.module('jbox')
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/directories', {
     templateUrl: 'directories/directories.html',
+    controller: 'DirectoryCtrl',
   });
 }])
 
+.controller('DirectoryCtrl', ['$scope', function($scope) {
+  $scope.directories = [{
+    'path': '/home/swoogan/Software/development/jbox/mp3s',
+    'recurse': 'Y'
+  }];
+}]);
+ 
