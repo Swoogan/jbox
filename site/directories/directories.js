081db91579e12ad58b1af3eb7d02556a8033d2a2
Colin Svingen <swoogan@gmail.com>
2015-03-13 22:10:21 -0400

    Directories are working dynamically

--- site/directories/directories.js
+++ site/directories/directories.js
@@ -12,7 +12,23 @@ angular.module('jbox')
 .controller('DirectoryCtrl', ['$scope', function($scope) {
   $scope.directories = [{
     'path': '/home/swoogan/Software/development/jbox/mp3s',
-    'recurse': 'Y'
+    'recurse': 'true'
   }];
+  $scope.path = '';
+  $scope.recurse = true;
+
+  $scope.addDirectory = function() {
+    $scope.directories.push({'path': $scope.path, 'recurse': $scope.recurse});
+  };  
+
+  $scope.delDirectory = function() {
+    var dir = $scope.directories;
+    for (var i = dir.length - 1; i >= 0; i--) {
+      if (dir[i].path === $scope.path) {
+	dir.splice(i, 1);
+      }
+    }
+    $scope.directories = dir;
+  };  
 }]);
  
